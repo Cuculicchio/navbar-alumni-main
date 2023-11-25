5d799471c0d9009c0b367f13c80a326372411b0d
Marcelo Cuculicchio <maccellbahia@gmail.com>
2023-11-25 20:55:51 -0300

    FOOTER2

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ let menuD = document.getElementById('nav-m');
 btnLogo.addEventListener('click', function(){
     menuD.classList.toggle('mostrarM');
 });
+let masDeportes = document.getElementById('masDeportes');
+let subMenu = document.getElementById('nav-m');
 
+masDeportes.addEventListener('click', function(){
+    subMenu.classList.toggle('subMenu');
+});
 
 //console.log(menuD)
