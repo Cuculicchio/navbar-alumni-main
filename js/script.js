310ecd2a277f1fbd696375b5f324fb7295450daf
Marcelo Cuculicchio <117914709+Cuculicchio@users.noreply.github.com>
2023-12-05 18:39:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,39 @@ masDeportes.addEventListener('click', function(){
 });
 
 //console.log(menuD)
+
+// tabla de posiciones automatica
+
+function ordenarTabla() {
+    var tabla, filas, switching, i, x, y, shouldSwitch;
+    tabla = document.getElementById("tablaPosiciones");
+    switching = true;
+  
+    while (switching) {
+      switching = false;
+      filas = tabla.rows;
+  
+      for (i = 1; i < (filas.length - 1); i++) {
+        shouldSwitch = false;
+  
+        x = filas[i].getElementsByTagName("TD")[2];
+        y = filas[i + 1].getElementsByTagName("TD")[2];
+  
+        if (Number(x.innerHTML) < Number(y.innerHTML)) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        filas[i].parentNode.insertBefore(filas[i + 1], filas[i]);
+        switching = true;
+      }
+    }
+  
+    // Actualiza la columna de posiciones
+    for (i = 1; i < filas.length; i++) {
+      filas[i].getElementsByTagName("TD")[0].innerHTML = i;
+    }
+  }
+  
+  ordenarTabla();
